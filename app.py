94b592611264b2b22f981bff9300089265bd8f5b
Kushal Matalia <kushalmatalia15.12@gmail.com>
2024-05-18 22:05:57 +0530

    Third commit -v2

--- app.py
+++ app.py
@@ -2,9 +2,6 @@ from flask import Flask, request, jsonify, render_template
 import pickle
 import numpy as np
 
-# dataset - https://www.kaggle.com/datasets/rakeshrau/social-network-ads
-
-
 app = Flask(__name__)
 model = pickle.load(open('nbclassifier.pkl', 'rb'))
 scaler = pickle.load(open('scaler.pickle', 'rb'))
@@ -13,30 +10,18 @@ scaler = pickle.load(open('scaler.pickle', 'rb'))
 def home():
     return render_template('index.html')
 
-@app.route('/predict',methods=['POST'])
+@app.route('/predict', methods=['POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    int_features = [int(x) for x in request.form.values()]
-    print("intial values -->", int_features)
-    pre_final_features = [np.array(int_features)]
-    final_features = scaler.transform(pre_final_features)
-    print("scaled values -->", final_features)
-    prediction = model.predict(final_features)   
-    print('prediction value is ', prediction[0])
-    if (prediction[0] == 1):
-        output = "True"
-    elif(prediction[0] == 0):
-        output = "False"
-    else:
-        output = "Not sure"
-        
-
-    # return rende_template('index.html', prediction_text='This user will buy from social network advertisement :: {}'.format(output))
-    # return jsonify({'prediction': int(prediction[0])})
+    gender = int(request.json['gender'])
+    age = int(request.json['age'])
+    salary = int(request.json['salary'])
+    
+    input_features = np.array([[gender, age, salary]])
+    scaled_features = scaler.transform(input_features)
+    prediction = model.predict(scaled_features)
+    output = "True" if prediction[0] == 1 else "False"
+    
     return jsonify({'prediction': output})
-
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5000, debug=True)
